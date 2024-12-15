2a144d87c5c27d8687bc50a84d4309f5a7c7a1ba
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2024-12-15 23:52:57 +0530

    Time: 38 ms (10.12%), Space: 75.7 MB (5.55%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// var findDisappearedNumbers = function(nums) {
+//     let newarray = []
+//     for(i=1;i<=nums.length;i++){
+//         if(nums.includes(i) == false)
+//             {newarray.push(i)}
+//         }
+//             return newarray
+//     }
+
+var findDisappearedNumbers = function(nums) {
+    const n = nums.length;
+    const numSet = new Set(nums); // Create a set from the array to store unique elements
+    return Array.from({ length: n }, (_, i) => i + 1).filter(num => !numSet.has(num));
+};
+
