a0d5919e055080cc6b440d3195183accd768684f
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2024-12-30 16:43:10 +0530

    Time: 87 ms (38.03%), Space: 58.8 MB (80.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -5,9 +5,5 @@
  */
 var findKthLargest = function(nums, k) {
     let sorted = nums.sort((a,b) => a-b)
-    let unique = [...new Set(sorted)]
-    // // let result = unique[k-1]
-    // console.log(sorted)
-    // console.log(unique)
     return sorted[(sorted.length) - k]
 };
