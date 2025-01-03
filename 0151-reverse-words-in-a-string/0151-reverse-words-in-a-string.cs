87601bcf0532e777506ea27714ad7c8299e85333
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-01-03 09:57:57 +0530

    Time: 16 ms (8.87%), Space: 43.1 MB (29.65%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -2,7 +2,7 @@ using System;
 using System.Text.RegularExpressions;
 public class Solution {
     public string ReverseWords(string s) {
-        string result = Regex.Replace(s, @"\s{2,}", " ").Trim();
+        string result = Regex.Replace(s, @"\s+", " ").Trim();
         string[] splitted = result.Split(" ");
         Array.Reverse(splitted);
         
