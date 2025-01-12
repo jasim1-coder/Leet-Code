3f59a2189d846e5c27ef45678bd006be2c1f7356
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-01-12 22:57:21 +0530

    Time: 0 ms (100%), Space: 44.1 MB (5.56%) - LeetHub

--- 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.cs
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.cs
@@ -2,14 +2,9 @@ public class Solution {
     public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
         string new1 = string.Join("",word1);
         string new2 =string.Join("",word2);
-        Console.WriteLine(new1);
-                Console.WriteLine(new2);
+        bool result = new1 == new2;
 
-        if(new1 == new2){
-            return true;
-        }else{
-            return false;
-        }
+    return result;
 
 
     }
