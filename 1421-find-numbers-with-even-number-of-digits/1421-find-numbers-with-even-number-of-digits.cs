97bbfa80a5c80345c70782584a60c13db947acae
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-01-18 21:57:49 +0530

    Time: 0 ms (100%), Space: 42.4 MB (29.4%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int FindNumbers(int[] nums) {
+        int count = 0 ;
+        foreach(int num in nums){
+            if(num.ToString().Length % 2 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
