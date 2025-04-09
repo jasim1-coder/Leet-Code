a1e6a28823a73614c87be630504381c4c9896d22
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-04-09 22:51:07 +0530

    Time: 0 ms (100%), Space: 31 MB (43.13%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsPowerOfFour(int n) {
+        
+        if (n <= 0)
+        {
+            return false;
+        }
+        
+        while (n > 1)
+        {
+            if (n % 4 != 0)
+            {
+
+                return false;
+
+            }
+
+            n /= 4;
+        }
+        return true;
+    }
+}
