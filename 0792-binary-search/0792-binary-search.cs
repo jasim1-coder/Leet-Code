9f17f52d12c1a25fefe43baa43ea16443db6477e
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-11-11 21:28:02 +0530

    Time: 0 ms (100%), Space: 53 MB (13.75%) - LeetHub

--- 0792-binary-search/0792-binary-search.cs
+++ 0792-binary-search/0792-binary-search.cs
@@ -1,15 +1,35 @@
 public class Solution {
+
+ public int Searchbin(int[] arr, int high, int low, int target){
+        if(low > high) return -1;
+
+        int middle = (low + high) / 2;
+
+        if(arr[middle] == target) return middle;
+        else if(arr[middle] > target){
+            return Searchbin(arr, middle - 1 , low , target);
+        }else{
+            return Searchbin(arr, high  , middle + 1 , target);
+        }
+ }
+
     public int Search(int[] nums, int target) {
         int low = 0 , high = nums.Length - 1 ;
-        while(low <= high){
-            int middle = (low + high)/2 ;
-            if(nums[middle] == target)return middle;
-            else if(nums[middle] > target){
-                high = middle - 1;
-            }else{
-                low = middle + 1;
-            }
-        }
-        return -1;
+        // while(low <= high){
+        //     int middle = (low + high)/2 ;
+        //     if(nums[middle] == target)return middle;
+        //     else if(nums[middle] > target){
+        //         high = middle - 1;
+        //     }else{
+        //         low = middle + 1;
+        //     }
+        // }
+        // return -1;
+        int result = Searchbin(nums,high, low, target);
+        return result;
+     
     }
-}
+
+ }
+
+    
