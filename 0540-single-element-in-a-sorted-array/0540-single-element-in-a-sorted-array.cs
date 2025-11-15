0fbdf1561c20cb9cf810adb425f5b69abe06d635
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-11-15 22:57:30 +0530

    Time: 0 ms (100%), Space: 49.6 MB (92.36%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int SingleNonDuplicate(int[] nums) {
+        int low = 1, high = nums.Length - 2 ;
+        if(high == -1)return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[high + 1] != nums[high]) return nums[high + 1];
+
+        while(low <= high){
+            int middle = (low + high) / 2;
+            if(nums[middle] != nums[middle - 1] && nums[middle] != nums[middle +1]) return nums[middle];
+
+            if((middle % 2 != 0 && nums[middle - 1] == nums[middle]) || (middle % 2 == 0 && nums[middle] == nums[middle + 1])){
+                low = middle + 1;
+            }else{
+                high = middle -1 ;
+            }
+        }
+        return -1;
+    }
+}
