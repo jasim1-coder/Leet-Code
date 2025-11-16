e64ac523edb362fdcec9a630ce557def3a6cb397
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-11-16 15:17:05 +0530

    Time: 0 ms (100%), Space: 47 MB (71.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cs
+++ 0268-missing-number/0268-missing-number.cs
@@ -1,11 +1,12 @@
 public class Solution {
     public int MissingNumber(int[] nums) {
         int n = nums.Length ;
-        for(int i = 0 ; i <= n ; i++){
-            if(!nums.Contains(i)){
-                return i;
-            }
+        int sum = 0 ;
+        int max = (n * (n+1))/2;
+        for(int i = 0 ; i < n ; i++){
+            sum += nums[i];
         }
-        return -1 ;
+        return max - sum ;
+    
     }
 }
