5d39f731708315da433f78881f04092dff43ced5
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2025-11-27 23:13:41 +0530

    Time: 1255 ms (17.49%), Space: 52.5 MB (31.58%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int SubarraySum(int[] nums, int k) {
+        int count = 0 ;
+          for(int i = 0 ; i < nums.Length ; i++){
+            int sum = 0 ;
+            for(int j = i; j< nums.Length ; j++){
+
+                sum += nums[j];
+                 if(sum == k){
+                    count += 1 ;
+                }
+            }
+          }
+          return count ;
+    }
+}
