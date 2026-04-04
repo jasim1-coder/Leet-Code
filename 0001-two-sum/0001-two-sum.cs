74829652c595ef6b0b2be96a64b7a32a95011227
Muhammed Jasim T <84397837+jasim1-coder@users.noreply.github.com>
2026-04-04 11:59:23 +0530

    Time: 4 ms (71.25%), Space: 50.1 MB (16.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int , int> store = new Dictionary<int , int >();
-        for(int i = 0 ; i < nums.Length ; i++){
-            int need = target - nums[i];
-            if(store.ContainsKey(need)){
-                return new int[] { store[need], i};
+        Dictionary<int, int> map = new Dictionary<int, int>();
+        for(int i = 0 ; i < nums.Length  ; i++){
+            int complement = target - nums[i];
+            if(map.ContainsKey(complement)){
+                return new int[] {map[complement], i};
             }
-            store[nums[i]] = i ;
+            map[nums[i]] = i;
         }
         return new int[2];
     }
